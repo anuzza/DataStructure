b43772d0d7281808f8d6fad32ed1db0b3e11ce37
anuzza <anuja3580@gmail.com>
2022-01-24 18:06:59 -0600

    the spread operator

--- script.js
+++ script.js
@@ -42,8 +42,72 @@ const restaurant = {
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
 };
 
+const arr = [7, 8, 9];
+const badNewaArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewaArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+console.log(1, 2, 7, 8, 9);
+
+//to pass multiple elements in a function
+// to spread out the elements of the array
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci']; // creating newa array from scratch
+console.log(newMenu);
+
+//takes all the elements from the array but does not create variables; can use it for values separated by commas.
+
+//create shallow copies of arrays
+
+//copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+//Join 2 arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+//Iterables: arrays, strings, maps, sets, but NOT OBJECTS!
+
+const str = 'Jonas';
+const letters = [...str, '', 'S.'];
+console.log(letters);
+console.log(...str);
+// console.log(`${...str} Schmedtmann`); does not work
+
+//real world example
+const ingredients = [
+  //   prompt(`Let's make pasta! Ingredient 1?`),
+  //   prompt('Ingredient 2?'),
+  //   prompt('Ingredient 3?'),
+];
+
+console.log(ingredients);
+
+restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+restaurant.orderPasta(...ingredients);
+
+//Objects
+const newRestaurant = { foundIn: 1998, ...restaurant, founder: 'Guiseppe' };
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+
+/*
+//Destructuring objects
 //the names should be same but no need to match the order
 restaurant.orderDelivery({
   time: '22:30',
