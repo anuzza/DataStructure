fb42345b42a698012d9acdb3dda33c85b9f7077d
anuzza <anuja3580@gmail.com>
2022-01-24 19:11:41 -0600

    the nullish coalescing operator

--- script.js
+++ script.js
@@ -55,6 +55,20 @@ const restaurant = {
   },
 };
 
+////////////////////////////////
+//The Nullish Coallescing operator
+
+//restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+//Nullish: null and undefined(Not 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+
+/*
+////////////////////////////////////// Short Circuiting (&& and ||)
+
 console.log('-------OR---------');
 // Logical operators:  Use any data type, return any data type, short-circuiting
 console.log(3 || 'Jonas');
