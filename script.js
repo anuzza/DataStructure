f61119bdfd1c5c10ba8e58fbeac7f06bd4521b49
anuzza <anuja3580@gmail.com>
2022-01-24 19:30:34 -0600

    logical assignment operators

--- script.js
+++ script.js
@@ -55,6 +55,43 @@ const restaurant = {
   },
 };
 
+///////////////////////////
+//Logical Assignment Operators
+
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+//OR aasignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+//concise
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+//nullish assignment operator
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+
+/*
+
 ////////////////////////////////
 //The Nullish Coallescing operator
 
